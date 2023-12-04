5e37ba8069db7b7fc000a3f51b77b9c6d64e28f9
bjacobsUNB <73720654+bjacobsUNB@users.noreply.github.com>
2023-12-03 23:25:58 -0400

    Added Missing ToDo

--- app/src/main/java/cs2063/groceryshopper/AddActivity.kt
+++ app/src/main/java/cs2063/groceryshopper/AddActivity.kt
@@ -97,6 +97,10 @@ class AddActivity : AppCompatActivity() {
         listOfEditorsGenerator.generateList(this, db, rawText)
 
 //        // Setup Archive / Delete for Items
+          // ToDo: Add the delete functionality to all the rows 
+          // (idk if it would be here or in the list generator, 
+          // i assume here as you would want to remove it from 
+          // a list in this activity)
 //        val listView : ListView = this.findViewById(R.id.tripList)
 //        listView.onItemLongClickListener = OnItemLongClickListener { _, item: View, _: Int, _ ->
 //            true
